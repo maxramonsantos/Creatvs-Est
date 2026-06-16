cf74d79f4eb2403e170873e72daa1accd46513c5
Max Ramon <138030293+maxramonsantos@users.noreply.github.com>
2026-06-16 06:29:18 +0000

    add style.css

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "<h1>Creaztvs-Est</h1><p>Flask está rodando!</p>"
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
